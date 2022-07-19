04945cc2126b4ec0e83bc00ee11030eb08975f07
Gauri Joshi <gauri728@gmail.com>
2022-07-18 19:44:28 -0700

    model tests pass

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+   has_many :recipe_ingredients 
+   has_many :ingredients, through: :recipe_ingredients
+
+   validates_presence_of :name
+   validates_presence_of :complexity
+   validates_presence_of :genre
 end
